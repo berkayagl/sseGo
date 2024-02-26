a6aa01c376d3bb3c8e7026accbbb7bcb845d6d3f
berkayagl <156705760+berkayagl@users.noreply.github.com>
2024-02-26 23:48:31 +0300

    Add files via upload

---
+++ sse.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+// SSEConnection struct holds the connection and message channel information
+type SSEConnection struct {
+	Connection http.ResponseWriter
+	Message    chan string
+}
+
+// sseStreamData struct holds the SSE connections and their corresponding message channels
+type sseStreamData struct {
+	Connections map[http.ResponseWriter]SSEConnection
+	sync.RWMutex
+}
+
+var sseData = sseStreamData{
+	Connections: make(map[http.ResponseWriter]SSEConnection),
+}
+
+func prepareHeaderForSSE(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func writeData(w http.ResponseWriter) (int, error) {
+	return fmt.Fprintf(w, "data: %s\n\n", <-sseData.Connections[w].Message)
+}
+
+func sseStream() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		prepareHeaderForSSE(w)
+
+		msgChan := make(chan string)
+		sseData.Lock()
+		sseData.Connections[w] = SSEConnection{
+			Connection: w,
+			Message:    msgChan,
+		}
+		sseData.Unlock()
+
+		defer func() {
+			sseData.Lock()
+			delete(sseData.Connections, w)
+			close(msgChan)
+			sseData.Unlock()
+		}()
+
+		flusher, _ := w.(http.Flusher)
+
+		for {
+			write, err := writeData(w)
+			if err != nil {
+				log.Println(err)
+			}
+			log.Println(write)
+			flusher.Flush()
+		}
+	}
+}
+
+func sseMessage(message string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sseData.RLock()
+		sc, ok := sseData.Connections[w]
+		sseData.RUnlock()
+
+		if ok {
+			sc.Message <- message
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/stream", sseStream())
+	http.HandleFunc("/send", sseMessage("you can put json in here as the data"))
+	http.HandleFunc("/right", sseMessage("right"))
+	http.HandleFunc("/left", sseMessage("left"))
+	log.Fatal("HTTP server error:", http.ListenAndServe("localhost:8080", nil))
+}
